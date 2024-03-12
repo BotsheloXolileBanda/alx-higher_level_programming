9611a60d2f9615b356ed89a3e9793f5512eff369
BotsheloXolileBanda <bxbanda@gmail.com>
2024-03-12 15:40:21 -0700

    fixed a logical error by including more hndlers for edge cases in task 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-lastdig = (abs(number) % 10)
-if lastdig > 5:
-    print(f"Last digit of {number} is {lastdig} and is greater than 5")
-elif lastdig == 0:
-    print(f"Last digtit of {number} is {lastdig} and is 0")
+if number < 0:
+    lastdig = ((number * -1) % 10)
+    if lastdig == 0:
+        print(f"Last digit of {number} is 0 and is 0")
+    else:
+        print(f"Last digit of {number} is -{lastdig} and is less than 6 and not 0")
 else:
-    print(f"Last digit of {number} is {lastdig} and is less than 6 and not 0")
+    lastdi = (number % 10)
+    if lastdi > 5:
+        print(f"Last digit of {number} is {lastdi} and is greater than 5")
+    elif lastdi == 0:
+        print(f"Last digit of {number} is 0 and is 0")
+    else:
+        print(f"Last digit of {number} is {lastdi} and is less than 6 and not 0")
