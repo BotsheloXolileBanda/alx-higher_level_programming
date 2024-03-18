73b363e37b36189253eccf015ced304c710290dd
BotsheloXolileBanda <bxbanda@gmail.com>
2024-03-17 23:22:39 -0700

    Fixed an edge case handling out bound index in task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@
 def element_at(mylist, idx):
     if idx < 0:
         return none
-    elif idx >= len(mylist):
+    elif idx > len(mylist):
         return none
     else:
         return mylist[idx]
