6513d0c6286e255ad48c68a9ccb5090fc33f4aa4
BotsheloXolileBanda <bxbanda@gmail.com>
2024-03-17 23:32:29 -0700

    Fixedan edge case for out of range indexes

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@
 def element_at(mylist, idx):
     if idx < 0:
         return None
-    elif idx > len(mylist):
+    elif idx >= len(mylist):
         return None
     else:
         return mylist[idx]
