783b9aaa19421d40f324f7e71b2b26cf0c79f45b
BotsheloXolileBanda <bxbanda@gmail.com>
2024-03-18 13:23:46 -0700

    Task6 done fixed formatting errro

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for x in range(len(matrix[i])):
-            print("{:d}".format(matrix[i][x]), end=" ")
-        print()
+    if not matrix:
+        print("", end="")
+    else:
+        for i in range(len(matrix)):
+            for x in range(len(matrix[i])):
+                if x == ((len(matrix)) - 1):
+                    print("{:d}".format(matrix[i][x]))
+                else:
+                    print("{:d}".format(matrix[i][x]), end=" ")
