fa8121e24820c5d1f8b2709acfa876e28d3d6144
BotsheloXolileBanda <bxbanda@gmail.com>
2024-03-17 23:28:59 -0700

    Was returning none instead oneNone in task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def element_at(mylist, idx):
-    if int(idx) < 0:
-        return none
-    elif int(idx) > len(mylist):
-        return none
+    if idx < 0:
+        return None
+    elif idx > len(mylist):
+        return None
     else:
         return mylist[idx]
