397bdde07809a2adc9a46bcc0166219614f572fc
BotsheloXolileBanda <bxbanda@gmail.com>
2024-03-17 22:48:36 -0700

    Fixed a pycodestyle violation

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def print_list_integer(mylist=[]):
-	 leng = len(mylist)
-	 for i in range(leng):
-             print("{:d}".format(mylist[i]))
+    leng = len(mylist)
+    for i in range(leng):
+        print("{:d}".format(mylist[i]))
