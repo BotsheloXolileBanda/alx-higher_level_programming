fdb76f26695616364a30090518c8516b5e5d4f57
BotsheloXolileBanda <bxbanda@gmail.com>
2024-03-18 18:48:37 -0700

    Task 9, finds the maximum integer in list and fixed pycodestyle violation

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -5,7 +5,7 @@ def max_integer(my_list=[]):
         return None
     else:
         temnu = my_list[0]
-        for i in range (len(my_list)):
+        for i in range(len(my_list)):
             if my_list[i] > temnu:
                 temnu = my_list[i]
         return temnu
